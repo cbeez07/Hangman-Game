28497feb2aa8cfbdd63a9acf7d559d78d78a4293
Chris Beesley <beesleychc@gmail.com>
2018-02-08 10:10:09 -0600

    complete JS, beginning css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,109 @@
-// choosing a random words
-// create an array for words.
+// naming all my variables
 
-var tvShows = [
-  "scrubs",
-  "firefly",
-  "community"
+var animals = [
+  "dog",
+  "cat",
+  "hippopotamus",
+  "ostrich",
+  "elephant",
+  "emu",
+  "giraffe",
+  "rabbit",
+  "zebra",
+  "newt",
+  "kangaroo"
 ];
 var space;
-var words = tvShows[Math.floor(Math.random() * tvShows.length)];
+var words;
 var wins = 0;
 var hangmanAnswer = [];
 var theWord = document.getElementById("currentWord")
-var wrongGuesses = 0;
-var letters = [];
-// function for picking a random word
+var guessesLeft = 7;
+var lettersUsersGuessed = [];
+var userGuess;
+
+var isAlpha = function(ch){
+  return /^[A-Z]$/i.test(ch);
+}
 
 
+
+// function to start the game
 function startUp() {
+  hangmanAnswer = [];
+  words = animals[Math.floor(Math.random() * animals.length)];
   for (var i = 0; i < words.length; i++) {
     hangmanAnswer[i] = "_";
   }
   space = hangmanAnswer.join(" ");
   document.getElementById("currentWord").innerHTML = space;
+  lettersUsersGuessed = [];
+  guessesLeft = 7;
+  document.getElementById('guesses').innerHTML = guessesLeft;
+  document.getElementById('letters').innerHTML = lettersUsersGuessed;
+  document.getElementById("win").innerHTML = wins;
 }
 
-function gameWin() {
-  wins++;
-  document.getElementById("win").innerHTML = wins;
-  alert('You Win');
+function gameLoss() {
+  alert('You have lost! The animal was ' + words);
   startUp();
 }
-// function lettersGuessed() {
-//   if () {
-//
-//   }
-// }
+
+function wrongGuessesLeft() {
+  --guessesLeft;
+  document.getElementById('guesses').innerHTML = guessesLeft;
+    if (guessesLeft < 1) {
+      gameLoss();
+    }
+}
+
+function lettersGuessed() {
+  lettersUsersGuessed.push(userGuess);
+  document.getElementById('letters').innerHTML = lettersUsersGuessed;
+}
+
+function correctGuess() {
+  for (var t = 0; t < words.length; t++) {
+    if (words[t] === userGuess) {
+      hangmanAnswer[t] = userGuess;
+      space = hangmanAnswer.join(' ');
+      document.getElementById("currentWord").innerHTML = space;
+
+    }
+  }
+}
+
+function checkGameWin() {
+  if (hangmanAnswer.indexOf("_") === -1) {
+    wins++;
+    alert('You Win!! The animal is ' + words);
+    startUp();
+  }
+}
 
 document.onkeyup = function(event) {
-  var userGuess = event.key;
-  if (event.keyCode >= 65 && event.keyCode <= 90){
+  userGuess = event.key;
+  if (isAlpha(event.key)) {
 
-    console.log('first');
-    for (var t = 0; t < words.length; t++) {
-      if (words[t] === userGuess) {
-        hangmanAnswer[t] = userGuess;
-        space = hangmanAnswer.join(' ');
-        document.getElementById("currentWord").innerHTML = space;
-      }
-      if (hangmanAnswer[t] <= 0){
-        gameWin();
-      };
-
-    // Everything I have tried adds the words.lenght rather than just one to wrongGuesses.
-    // else {
-    //   wrongGuesses++ ;
-    //   document.getElementById("guesses").innerHTML = wrongGuesses;
-    // }
-    };
-  }
+  
+  // if (event.keyCode >= 65 && event.keyCode <= 90) {
+    if (words.indexOf(userGuess) !== -1) {
+      // console.log(first);
+      correctGuess();
+      checkGameWin();
+    }
+    else if (lettersUsersGuessed.indexOf(userGuess) === -1) {
+      wrongGuessesLeft();
+      lettersGuessed();
+    }
+  };
 };
+    // User guessed correctly
+
+
+// Get user's guess
+// For Each blank
+//   if guess is correct
+// if letter isnt in guessedLetters
+//   add letter to guessedLetters
+//   subtract from guesses
